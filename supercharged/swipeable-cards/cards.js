25c0f2b8588c3200076c5d96fa478cd356fee767
Zakaria Amarifi <z.amarifi@dm73.net>
2016-09-28 17:58:06 +0100

    start draging cards

--- supercharged/swipeable-cards/cards.js
+++ supercharged/swipeable-cards/cards.js
@@ -1,7 +1,11 @@
 class Cards {
     constructor() {
         this.cards = document.querySelectorAll('.card');
-        console.log('we have ' + this.cards.length + ' cards');
+        this.target   = null;
+        this.pageX    = 0;
+        this.currentX = 0;
+        this.limitDistance = 0;
+        // console.log('we have ' + this.cards.length + ' cards');
 
         this.addEventsListener();
     }
@@ -13,15 +17,32 @@ class Cards {
     }
 
     onStart(evt) {
-        console.log('action Start', evt);
+        if (!evt.target.classList.contains('card')) {
+            return;
+        }
+        // console.log('action Start', evt);
+        this.target = evt.target;
+        this.currentX = evt.pageX || evt.touches[0].pageX;
+        this.limitDistance = Math.ceil(this.target.clientWidth / 2);
     }
 
     onMove(evt) {
-        console.log('action Move', evt);
+        if (this.target !== evt.target) {
+            return;
+        }
+        // console.log('action Move', evt);
+        this.pageX = evt.pageX || evt.touches[0].pageX;
+        let distanceX = this.pageX - this.currentX;
+        this.target.style.transform = 'translate(' + (this.pageX < (window.innerWidth - 10) ? distanceX : window.innerWidth - 10 - this.currentX) + 'px, 0)';
     }
 
     onEnd(evt) {
-        console.log('action End', evt);
+        if (this.target !== evt.target) {
+            return;
+        }
+        // console.log('action End', evt);
+        this.target.style.transform = 'translate(0, 0)';
+        this.target = null;
     }
 
     onUpdate() {
