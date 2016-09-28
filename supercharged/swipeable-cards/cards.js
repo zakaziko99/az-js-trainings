4a1cdd56d63955ef8d413dd256df5b43f53bde3f
Zakaria Amarifi <z.amarifi@dm73.net>
2016-09-28 14:09:04 +0100

    setting the touch events

--- supercharged/swipeable-cards/cards.js
+++ supercharged/swipeable-cards/cards.js
@@ -2,6 +2,30 @@ class Cards {
     constructor() {
         this.cards = document.querySelectorAll('.card');
         console.log('we have ' + this.cards.length + ' cards');
+
+        this.addEventsListener();
+    }
+
+    addEventsListener() {
+        window.addEventListener('touchstart', this.onStart);
+        window.addEventListener('touchmove',  this.onMove);
+        window.addEventListener('touchend',   this.onEnd);
+    }
+
+    onStart(evt) {
+        console.log('action Start', evt);
+    }
+
+    onMove(evt) {
+        console.log('action Move', evt);
+    }
+
+    onEnd(evt) {
+        console.log('action End', evt);
+    }
+
+    onUpdate() {
+        //
     }
 }
 
